73e12cd23f1d90c1374c90cc6db0ccb99ec0d8bf
jukemori <jukemori@gmail.com>
2023-09-25 14:14:43 +0900

    add suggestion controller

---
+++ app/Http/Controllers/SuggestionController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SuggestionController extends Controller
+{
+    public function index()
+    {
+        $suggestions = Suggestion::all();
+        return response()->json($suggestions);
+    }
+
+    public function show(Suggestion $suggestion)
+    {
+        return response()->json($suggestion);
+    }
+
+    public function store(Request $request)
+    {
+        $suggestion = Suggestion::create($request->all());
+        return response()->json($suggestion, 201);
+    }
+
+    public function update(Request $request, Suggestion $suggestion)
+    {
+        $suggestion->update($request->all());
+        return response()->json($suggestion);
+    }
+
+    public function destroy(Suggestion $suggestion)
+    {
+        $suggestion->delete();
+        return response()->json(['message' => 'Suggestion deleted']);
+    }
+}
