c2f0c7633d553c1a8263a093b1ee8f72fcade975
jukemori <jukemori@gmail.com>
2023-09-25 14:08:35 +0900

    add project controller

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return response()->json($projects);
+    }
+
+    public function show(Project $project)
+    {
+        return response()->json($project);
+    }
+
+    public function store(Request $request)
+    {
+        $project = Project::create($request->all());
+        return response()->json($project, 201);
+    }
+
+    public function update(Request $request, Project $project)
+    {
+        $project->update($request->all());
+        return response()->json($project);
+    }
+
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return response()->json(['message' => 'Project deleted']);
+    }
+}
